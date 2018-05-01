d7c83935cd1a7c319f7d59ffcfe4af18999f1bc0
George Albertson <gdea73@gmail.com>
2018-05-01 00:23:02 -0500

    removed obsolete game_state struct

--- inc/agents.h
+++ inc/agents.h
@@ -5,12 +5,6 @@
 #include "utils.h"
 #include "game.h"
 
-struct game_state {
-	struct stats *stats;
-	char *board;
-	uint8_t game_over;
-};
-
 struct game *random_play_single_game(void);
 
 struct game *greedy_play_single_game(void);
